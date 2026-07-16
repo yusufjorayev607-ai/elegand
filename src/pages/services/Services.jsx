f0833faf62139480c74219d58ed385c6de682b51
yusufjorayev607-ai <yusufjorayev607@gmail.com>
2026-07-16 14:35:39 +0500

    a

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'react-i18next'
 import { useFetch } from '../../hooks/useFetch'
-import Loading from '../loading/loading'
+import Loading from '../loading/Loading'
 import './services.css'
 
 function Services() {
